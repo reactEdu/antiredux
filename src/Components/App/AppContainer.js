9982649c9672cc65be4ef4bd56808565a1f9c6ae
macbook_of_L <ds2lvg@gmail.com>
2019-12-27 13:20:09 +0900

    Provider로 Consumer한테 데이터 제공하기

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,15 +3,25 @@ import AppPresenter from "./AppPresenter";
 import Store from "store";
 
 class AppContainer extends Component {
-  state = {
-    messgage: "Hello"
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        messgage: "Bye"
-      })
-    }, 2000)
+  // Provider에 추가하고 싶은 함수는 constructor 내부에 존재해야함
+  // 인스턴스가 생성되었을때 Store가 value를 얻기때문
+  constructor(props) {
+    super(props);
+    this._changeMessage = () => {
+      if(this.state.messgage === "Hello") {
+        this.setState({
+          messgage: "Bye Bye"
+        });
+      } else {
+        this.setState({
+          messgage: "Hello"
+        });
+      }
+    }
+    this.state = {
+      messgage: "Hello",
+      changeMessage: this._changeMessage
+    }
   }
   render() {
     return (
