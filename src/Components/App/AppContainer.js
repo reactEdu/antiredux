f22d2e3d0f4da0933a30aca912c666f9891a36e5
macbook_of_L <ds2lvg@gmail.com>
2019-12-27 11:51:04 +0900

    Provider와 Consumer

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,9 +3,19 @@ import AppPresenter from "./AppPresenter";
 import Store from "store";
 
 class AppContainer extends Component {
+  state = {
+    messgage: "Hello"
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        messgage: "Bye"
+      })
+    }, 2000)
+  }
   render() {
     return (
-      <Store.Provider>
+      <Store.Provider value={this.state}>
         <AppPresenter />
       </Store.Provider>
     );
